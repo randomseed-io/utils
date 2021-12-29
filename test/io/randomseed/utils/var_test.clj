9c9d365a7767fe99f47d873296b079d21719ec6f
Paweł Wilk <siefca@gnu.org>
2021-12-29 11:14:12 +0100

    Added test stub for utils.var

---
+++ test/io/randomseed/utils/var_test.clj
@@ -0,0 +1,18 @@
+(ns
+
+    ^{:doc    "Random utils, var tests."
+      :author "Paweł Wilk"
+      :added  "1.0.0"
+      :no-doc true}
+
+    io.randomseed.utils.var-test
+
+  (:require [clojure.spec.alpha              :as               s]
+            [clojure.spec.gen.alpha          :as             gen]
+            [orchestra.spec.test             :as              st]
+            [io.randomseed.utils             :refer         :all]
+            [expound.alpha                   :as         expound]
+            [io.randomseed.utils.var         :as             var]))
+
+(s/check-asserts true)
+
