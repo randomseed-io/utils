8623ca1a9c2ab1cd9e659036629d1ef2bbe165a6
Paweł Wilk <siefca@gnu.org>
2022-04-03 18:11:49 +0200

    Added crypto/ask-pass

--- src/io/randomseed/utils/crypto.clj
+++ src/io/randomseed/utils/crypto.clj
@@ -78,3 +78,50 @@
   ([prompt]
    (when-some [c ^java.io.Console (System/console)]
      (some-str (.readLine ^java.io.Console c prompt nil)))))
+
+(defn ask-pass
+  "Ask user for a password with confirmation. Repeats until two passwords are the same
+  and are not empty. Keyword arguments can be given to configure behavior:
+  `prompt` (message displayed when asking for first password),
+  `confirm-prompt` (message displayed when asking for the same password again),
+  `not-match-msg` (message displayed when passwords do not match),
+  `empty-msg` (message displayed when the entered password is empty),
+  `retries` (number of retries before quitting the loop; when set to `nil` or not
+  given, it will continue indefinitely),
+  `confirmation?` (requires password to be re-entered for confirmation, defaults to `true`),
+  `allow-empty?` (allows the entered password to be an empty string; defaults to `false`),
+  `empty-nil?` (returns `nil` instead of an empty string when on empty password; defaults to `false`),
+  `empty-quits?` (short-circuits on any empty password and returns `nil`; defaults to `false`),
+  `empty-quits-nil?` (returns `nil` when quitting on empty password; defaults to `true`).
+  Returns the entered password or `nil`."
+  ([& {:keys [prompt
+              confirm-prompt
+              not-match-msg
+              empty-msg
+              retries
+              allow-empty?
+              empty-nil?
+              empty-quits?
+              empty-quits-nil?
+              confirmation?]
+       :or   {allow-empty?     false
+              empty-nil?       false
+              empty-quits?     false
+              empty-quits-nil? true
+              confirmation?    true
+              prompt           "Enter password: "
+              confirm-prompt   "Repeat password: "
+              not-match-msg    "Passwords do not match."
+              empty-msg        "Password is empty."}}]
+   (loop [counter (when retries (unchecked-int (if (pos-int? retries) retries 1)))]
+     (when-not (and counter (zero? counter))
+       (let [p1 (read-pwd prompt)]
+         (if (and (nil? p1) empty-quits?)
+           (when-not empty-quits-nil? "")
+           (let [p2      (if confirmation? (read-pwd confirm-prompt) p1)
+                 counter (when counter (unchecked-dec-int counter))]
+             (if (and (nil? p2) empty-quits?)
+               (when-not empty-quits-nil? "")
+               (if (= p1 p2)
+                 (or p1 (if allow-empty? (when-not empty-nil? "") (do (println empty-msg) (recur counter))))
+                 (do (println not-match-msg) (recur counter)))))))))))
