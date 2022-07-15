0a22a4caf51dec605fb943af528217bc40d76a14
Paweł Wilk <siefca@gnu.org>
2022-07-15 16:58:05 +0200

    Common validators improved.

--- src/io/randomseed/utils/validators/common.clj
+++ src/io/randomseed/utils/validators/common.clj
@@ -11,79 +11,119 @@
   (:import [org.apache.commons.validator.routines
             EmailValidator
             UrlValidator
-            DateValidator])
+            DateValidator]
+           [java.util Locale])
 
-  (:require
-   [trptr.java-wrapper.locale :as          l]
-   [phone-number.core         :as      phone]
-   [io.randomseed.utils.map   :as        map]
-   [io.randomseed.utils       :refer    :all]))
+  (:require [trptr.java-wrapper.locale :as          l]
+            [phone-number.core         :as      phone]
+            [io.randomseed.utils.map   :as        map]
+            [io.randomseed.utils       :refer    :all]))
 
 ;;
 ;; e-mail
 ;;
 
 (defn valid-email?
-  (^Boolean [address]
-   (valid-email? address false false))
-  (^Boolean [address ^Boolean allow-local]
-   (valid-email? address allow-local false))
-  (^Boolean [address ^Boolean allow-local ^Boolean allow-tld]
-   (and
-    (string? address)
-    (when-some [v (.getInstance EmailValidator allow-local allow-tld)]
-      (.isValid v address)))))
+  (^Boolean [e]
+   (valid-email? e false true))
+  (^Boolean [e allow-local]
+   (valid-email? e allow-local true))
+  (^Boolean [e allow-local allow-tld]
+   (boolean
+    (when-some [e (some-str e)]
+      (.isValid ^EmailValidator (EmailValidator/getInstance ^Boolean (boolean allow-local)
+                                                            ^Boolean (boolean allow-tld))
+                ^String e)))))
 
-(defn ^Boolean valid-email-domain?
-  ([address]
-   (valid-email-domain? address false false))
-  ([address ^Boolean allow-local]
-   (valid-email-domain? address allow-local false))
-  ([address ^Boolean allow-local ^Boolean allow-tld]
-   (and
-    (string? address)
-    (when-some [v (.getInstance EmailValidator allow-local allow-tld)]
-      (.isValidDomain v address)))))
+(defn valid-email-domain?
+  (^Boolean [e]
+   (valid-email-domain? e false true))
+  (^Boolean [e allow-local]
+   (valid-email-domain? e allow-local true))
+  (^Boolean [e allow-local allow-tld]
+   (boolean
+    (when-some [e (some-str e)]
+      (.isValidDomain ^EmailValidator (EmailValidator/getInstance ^Boolean (boolean allow-local)
+                                                                  ^Boolean (boolean allow-tld))
+                      ^String e)))))
 
 (defn valid-email-local-part?
-  (^Boolean [address]
-   (valid-email-local-part? address false false))
-  (^Boolean [address ^Boolean allow-local]
-   (valid-email-local-part? address allow-local false))
-  (^Boolean [address ^Boolean allow-local ^Boolean allow-tld]
-   (and
-    (string? address)
-    (when-some [v (.getInstance EmailValidator allow-local allow-tld)]
-      (.isValidUser v address)))))
+  (^Boolean [e]
+   (valid-email-local-part? e false true))
+  (^Boolean [e allow-local]
+   (valid-email-local-part? e allow-local true))
+  (^Boolean [e allow-local allow-tld]
+   (boolean
+    (when-some [e (some-str e)]
+      (.isValidUser ^EmailValidator (EmailValidator/getInstance ^Boolean (boolean allow-local)
+                                                                ^Boolean (boolean allow-tld))
+                    ^String e)))))
 
 ;;
 ;; date
 ;;
 
-(defn ^Boolean valid-date?
-  ([value]
-   (and
-    (string? value)
-    (when-some [v (.getInstance DateValidator)]
-      (.isValid v value))))
-  ([value pattern]
-   (and
-    (string? value)
-    (string? pattern)
-    (when-some [v (.getInstance DateValidator)]
-      (.isValid v value pattern))))
-  ([value pattern ^java.util.Locale locale]
-   (and
-    (string? value)
-    (when-some [v (.getInstance DateValidator)]
-      (if (nil? pattern)
-        (.isValid v value (l/locale locale))
-        (and (string? pattern) (.isValid v value pattern (l/locale locale))))))))
+(defn valid-date?
+  (^Boolean [value]
+   (boolean
+    (when-some [value (some-str value)]
+      (.isValid ^DateValidator (.getInstance DateValidator) ^String value))))
+  (^Boolean [value pattern]
+   (boolean
+    (when-some [value (some-str value)]
+      (if-some [pattern (some-str pattern)]
+        (.isValid ^DateValidator (.getInstance DateValidator)
+                  ^String value ^String pattern)
+        (.isValid ^DateValidator (.getInstance DateValidator)
+                  ^String value)))))
+  (^Boolean [value pattern locale]
+   (boolean
+    (when-some [value (some-str value)]
+      (if-some [pattern (some-str pattern)]
+        (if locale
+          (.isValid ^DateValidator (.getInstance DateValidator)
+                    ^String value ^String pattern ^Locale (l/locale locale))
+          (.isValid ^DateValidator (.getInstance DateValidator)
+                    ^String value ^String pattern))
+        (if locale
+          (.isValid ^DateValidator (.getInstance DateValidator)
+                    ^String value
+                    ^Locale (l/locale locale))
+          (.isValid ^DateValidator (.getInstance DateValidator)
+                    ^String value)))))))
 
 ;;
 ;; phone number
 ;;
 
 (defn valid-phone?
-  ^Boolean [number]
-  (phone/valid? number))
+  (^Boolean [p]    (phone/valid? p))
+  (^Boolean [p dr] (phone/valid? p dr)))
+
+(defn valid-regular-phone?
+  (^Boolean [p]
+   (and (phone/valid? p)
+        (not (let [pn (phone/number p)]
+               (or (phone/is-short? pn)
+                   (contains? #{:phone-number.type/toll-free
+                                :phone-number.type/premium-rate
+                                :phone-number.type/shared-cost}
+                              (phone/type pn)))))))
+  (^Boolean [p dr]
+   (and (phone/valid? p dr)
+        (not (let [pn (phone/number p dr)]
+               (or (phone/is-short? pn)
+                   (contains? #{:phone-number.type/toll-free
+                                :phone-number.type/premium-rate
+                                :phone-number.type/shared-cost}
+                              (phone/type pn))))))))
+
+;;
+;; url
+;;
+
+(defn valid-url?
+  ^Boolean [u]
+  (boolean
+   (when-some [u (some-str u)]
+     (.isValid ^UrlValidator (UrlValidator/getInstance) ^String u))))
