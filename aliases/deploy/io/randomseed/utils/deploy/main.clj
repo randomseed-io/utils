eba540916442085959f8dc48d68151f94c8cb078
Paweł Wilk <siefca@gnu.org>
2026-01-18 21:41:32 +0100

    Deployer fixed to accept POM file location

--- aliases/deploy/io/randomseed/utils/deploy/main.clj
+++ aliases/deploy/io/randomseed/utils/deploy/main.clj
@@ -10,12 +10,13 @@
     :sign-key-id '\"<fingerprint>\"'
   Env:
     CLOJARS_USERNAME, CLOJARS_PASSWORD (already in bin/deploy)"
-  [{:keys [artifact sign? sign-key-id installer]
+  [{:keys [artifact sign? sign-key-id installer pom-file]
     :or   {sign?     true
            installer :remote}}]
   (when-not artifact
     (throw (ex-info "Missing :artifact (path to jar)." {})))
   (dd/deploy (cond-> {:installer      installer
                       :artifact       artifact
+                      :pom-file       pom-file
                       :sign-releases? (boolean sign?)}
                sign-key-id (assoc :sign-key-id sign-key-id))))
